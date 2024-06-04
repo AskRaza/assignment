46155d949a74fbb269ac1b2ce1799d9523ca4536
Ahmed Raza <160354933+AskRaza@users.noreply.github.com>
2024-06-04 20:09:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var famousPersonName = "Albert Einstien";
+var message = "once said, “A person who never made a mistake never tried anything new.”";
+console.log(famousPersonName, message);
---
+++ main.ts
@@ -0,0 +1,5 @@
+let famousPersonName = "Albert Einstien";
+
+let message = "once said, “A person who never made a mistake never tried anything new.”";
+
+console.log(famousPersonName , message);
